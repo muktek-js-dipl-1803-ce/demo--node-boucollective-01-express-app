19d7ab9a41b51bff44220c7552395e23d7f9f0e4
tphdev <tphdevdrop@gmail.com>
2018-09-02 23:23:45 -0500

    adding

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 const express = require('express')
-const pageRouter = require('./src/routes/pageRouter.js')
-const apiRouter = require('./src/routes/apiRouter.js')
 
-const app = express()
+app = express()
 const PORT = 3000
-
-
-
-app.use('/api', apiRouter)
-app.use('/', pageRouter)
 
 
 app.use((req, res)=>{
